33274c99f46c68e9c6d476b7907daf7d34e88862
ignalpha4 <shubhambankar444@gmail.com>
2023-03-26 23:24:55 +0530

    updated

--- AI/AI1_DFS_BFS.cpp
+++ AI/AI1_DFS_BFS.cpp
@@ -2,47 +2,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class node {
+class node
+{
 public:
 	int data;
 	node *left, *right;
 };
 
-
-void printCurrentLevel(node* root, int level)
+void printCurrentLevel(node *root, int level)
 {
 	if (root == NULL)
 		return;
 	if (level == 1)
 		cout << root->data << " ";
-	else if (level > 1) {
+	else if (level > 1)
+	{
 		printCurrentLevel(root->left, level - 1);
 		printCurrentLevel(root->right, level - 1);
 	}
 }
 
-int height(node* node)
+int height(node *node)
 {
 	if (node == NULL)
 		return 0;
-	else {
-		
+	else
+	{
+
 		int lheight = height(node->left);
 		int rheight = height(node->right);
 
-		
-		if (lheight > rheight) {
+		if (lheight > rheight)
+		{
 			return (lheight + 1);
 		}
-		else {
+		else
+		{
 			return (rheight + 1);
 		}
 	}
 }
-node* newNode(int data)
+node *newNode(int data)
 {
-	node* Node = new node();
+	node *Node = new node();
 	Node->data = data;
 	Node->left = NULL;
 	Node->right = NULL;
@@ -50,8 +52,7 @@ node* newNode(int data)
 	return (Node);
 }
 
-
-void printLevelOrder(node* root)  //bfs
+void printLevelOrder(node *root) // bfs
 {
 	int h = height(root);
 	int i;
@@ -59,34 +60,31 @@ void printLevelOrder(node* root)  //bfs
 		printCurrentLevel(root, i);
 }
 
-
-void printInorder(node* node)  //dfs
+void printInorder(node *node) // dfs
 {
-    if (node == NULL)
-        return;
- 
-    printInorder(node->left);
- 
-    
-    cout << node->data << " ";
- 
-    printInorder(node->right);
-}
+	if (node == NULL)
+		return;
 
+	printInorder(node->left);
+
+	cout << node->data << " ";
+
+	printInorder(node->right);
+}
 
 int main()
 {
-	node* root = newNode(20);
+	node *root = newNode(20);
 	root->left = newNode(8);
 	root->right = newNode(22);
 	root->left->left = newNode(4);
 	root->left->right = newNode(12);
 
-	cout << "BFS"<<endl;
+	cout << "BFS" << endl;
 	printLevelOrder(root);
-	
-	cout<<"\nDFS"<<endl;
-	
+
+	cout << "\nDFS" << endl;
+
 	printInorder(root);
 
 	return 0;
